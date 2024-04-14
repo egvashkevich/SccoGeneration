3b37026135f347091d2f9836bbe268ef51107a1f
Vladislav Meshkov <mvs_1m@bk.ru>
2024-04-14 14:22:36 +0300

    add client info structure

--- scco/ml_generation/ml_models/main.py
+++ scco/ml_generation/ml_models/main.py
@@ -1,10 +1,27 @@
 import generation
 
-# example
-model = generation.GenerateGateWrapper()
-info = generation.MlClientInfo()
-info.current_message = "Добрый день, мне нужен разработчик на Питоне, \
-    для разработки сайта под интернет-магазин"
+import make_prompts
 
-ans = model.generate_offer_text(info)
+
+client = make_prompts.SystemPromptData()
+client.company_name = 'Интеллект'
+client.channel_name = 'Репетитор'
+client.services = [
+    'Английский (до уровня C2 и подготовка к экзаменам SAT, IELTS)',
+    'Математика (олимпиадная и школьная программа)',
+    'Физика (олимпиадная, школьная и вузовская программы)',
+    'Испанский (до уровня B2)',
+]
+client.specific = [
+    "Специалисты с 10 летним стажем",
+    "Опыт работы с детьми всех возрастов",
+]
+
+
+model = generation.GenerateGateWrapper()
+info = generation.MLRequestInfo()
+
+info.current_message = "Подскажите хорошего репетиора по математике. Дочь не успевает за школьной программой. 9 класс. Физико-математическое направление"
+
+ans = model.generate_offer_text(info, client)
 print(ans.main_text)
